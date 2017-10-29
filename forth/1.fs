fed627366d6739d51817da1e6bc8a39827e61433
Ed Ye <ye.edward.zhijia@gmail.com>
2017-10-29 01:56:10 -0400

    Fixed up 1.fs to work in gforth.

--- forth/1.fs
+++ forth/1.fs
@@ -4,7 +4,7 @@
 
 \ Tools
 
-: >UPC 95 AND ; \ what does this do?
+\ : >UPC 95 AND ; \ uppercase
 
 : tab 4 spaces ; \ don't know what tab character is in forth
 
@@ -12,22 +12,22 @@
 : getchar ( -- ) EKEY TO Look ;
 
 \ report an error
-: error ( c-addr -- ) CR 7 EMIT ." Error: " TYPE ." ." ;
+: error ( c-addr u -- ) CR 7 EMIT ." Error: " TYPE ." ." ;
 
 \ report an error and stops
-: aborts ( c-addr --) error ABORT ;
+: aborts ( c-addr u --) error ABORT ;
 
 \ report what was expected
-: expected ( c-addr -- )
-    pad place \ puts the c-addr in storage
+: expected ( c-addr u -- )
+    pad place \ put into pad
     s"  Expected" pad +place \ concatenate strings and store in pad
-    pad aborts ; \ send pad to aborts
+    pad count aborts ; \ send pad to aborts
 
 \ recognize an alpha character
-: alpha? ( char -- tf ) >UPC 'A' 'Z' 1+ WITHIN ;
+: alpha? ( char -- tf ) TOUPPER 'A' 'Z' 1+ WITHIN ;
 
 \ recognize a decimal digit
-: digit? ( char -- tf ) '0' '9' 1+ WITHIN ;
+\ : digit? ( char -- tf ) '0' '9' 1+ WITHIN ;
 
 \ output a string with tab
 : emits ( c-addr u -- ) tab EMIT TYPE ;
@@ -38,13 +38,34 @@
 \ initialize
 : init ( -- ) getchar ;
 
-\ Specifics
+\ Specifics ---------------------------------
 
 \ match a specific input character
+: match ( char -- )
+    DUP Look =
+    IF DROP getchar
+    ELSE
+        \ error, don't match 
+        20 allocate drop \ allocate string, pop error value
+        ''' over c$+! \ append ' character
+        Look over c$+! \ append Look character
+        ''' over c$+! \ append ' character
+        dup
+        $@ expected 
+    ENDIF ;
 
 \ get an identifier
+: getname ( -- char )
+    Look alpha? 0=
+    IF s" Name" expected ENDIF
+    Look TOUPPER
+    getchar ;
 
 \ get a number
+: getnum ( -- char )
+    Look digit? 0= IF s" Integer" expected ENDIF
+    Look '0' -
+    getchar ;
 
 \ Main Program
 : cradle ( -- ) init ;
